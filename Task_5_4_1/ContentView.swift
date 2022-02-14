dd754afa8e6027b966e65410ebaeb991a1b1a6e6
Legenda759 <oripov1994@gmail.com>
2022-02-14 10:41:54 +0500

    Initial Commit

---
+++ Task_5_4_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task_5_4_1
+//
+//  Created by Legenda_759 on 14/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task_5_4_1/Task_5_4_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_5_4_1App.swift
+//  Task_5_4_1
+//
+//  Created by Legenda_759 on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_5_4_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
